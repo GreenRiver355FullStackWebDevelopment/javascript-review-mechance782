a2035bd3c1b66e1d215087e9bf47436c5ee5459b
Madeleine Chance <mechance782@gmail.com>
2025-04-02 13:58:52 -0700

    created seperate function for rendering a single review

--- src/index.js
+++ src/index.js
@@ -40,33 +40,39 @@ window.onload = () => {
   </div> 
 */
 function renderReviews(reviews){
-  const section = document.querySelector(".reviews");
   reviews.forEach(review => {
-    // creating elements and assigning attributes
-    const container = document.createElement("div");
-    container.className = "review_container";
-    const image = document.createElement("img");
-    image.src = review.image;
-    const reviewTextDiv = document.createElement("div");
-    const username = document.createElement("p");
-    const starRating = document.createElement("p");
-    const content = document.createElement("p");
-
-    // adding content to necessary elements
-    username.textContent = review.username;
-    starRating.textContent = review.star;
-    content.textContent = review.review;
-
-    // connecting elements to each other
-    container.appendChild(image);
-    container.appendChild(reviewTextDiv);
-    reviewTextDiv.appendChild(username);
-    reviewTextDiv.appendChild(starRating);
-    reviewTextDiv.appendChild(content);
-
-    // connect elements to dom
-    section.appendChild(container);
+    renderReview(review);
   });
+}
+
+function renderReview(review){
+  // query element in dom
+  const section = document.querySelector(".reviews");
+
+  // creating elements and assigning attributes
+  const container = document.createElement("div");
+  container.className = "review_container";
+  const image = document.createElement("img");
+  image.src = review.image;
+  const reviewTextDiv = document.createElement("div");
+  const username = document.createElement("p");
+  const starRating = document.createElement("p");
+  const content = document.createElement("p");
+
+  // adding content to necessary elements
+  username.textContent = review.username;
+  starRating.textContent = review.star;
+  content.textContent = review.review;
+
+  // connecting elements to each other
+  container.appendChild(image);
+  container.appendChild(reviewTextDiv);
+  reviewTextDiv.appendChild(username);
+  reviewTextDiv.appendChild(starRating);
+  reviewTextDiv.appendChild(content);
+
+  // connect elements to dom
+  section.appendChild(container);
 }
 // calculate and render star average in class .starRating
 // ex: Star Rating: 5
